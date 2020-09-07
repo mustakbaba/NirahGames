e3b4224c19c702eaf56fd4c8441451f73334b2e2
mustakbaba <mustafaakbaba@trakya.edu.tr>
2020-09-07 15:39:08 +0300

    Initial commit

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speedOfRotation;
+    public Transform pivLeft, pivRight, pivForward, pivBack, center;
+    bool isRotating = false;
+    public GameObject mobileCanvas;
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (!isRotating)
+        {
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                StartCoroutine(Move("left"));
+
+                isRotating = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                StartCoroutine(Move("right"));
+                isRotating = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                StartCoroutine(Move("forward"));
+                isRotating = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                StartCoroutine(Move("back"));
+                isRotating = true;
+            }
+
+        }
+
+
+
+
+#elif UNITY_ANDROID
+        mobileCanvas.SetActive(true);
+#endif
+    }
+    public void ButtonLeft()
+    {
+        if (!isRotating)
+            StartCoroutine(Move("left"));
+        isRotating = true;
+    } 
+    public void ButtonRight()
+    {
+        if (!isRotating)
+            StartCoroutine(Move("right"));
+        isRotating = true;
+    } 
+    public void ButtonForward()
+    {
+        if (!isRotating)
+            StartCoroutine(Move("forward"));
+        isRotating = true;
+    } 
+    public void ButtonBack()
+    {
+        if (!isRotating)
+            StartCoroutine(Move("back"));
+        isRotating = true;
+    }
+    IEnumerator Move(string where)
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            switch (where)
+            {
+                case "left":
+                    gameObject.transform.RotateAround(pivLeft.transform.position, Vector3.forward, 3);
+                    break;
+                case "right":
+                    gameObject.transform.RotateAround(pivRight.transform.position, Vector3.back, 3);
+                    break;
+                case "forward":
+                    gameObject.transform.RotateAround(pivForward.transform.position, Vector3.right, 3);
+                    break;
+                case "back":
+                    gameObject.transform.RotateAround(pivBack.transform.position, Vector3.left, 3);
+                    break;
+
+
+            }
+            yield return new WaitForSeconds(speedOfRotation);
+        }
+        center.transform.position = gameObject.transform.position;
+        isRotating = false;
+    }
+
+}
